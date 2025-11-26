dcc8c94b18bc8e3a0073e5723fe4721e303574f6
VaibhavPatel30 <vaibhavpatel@promactinfo.com>
2025-11-26 15:09:30 +0530

    Initial commit

---
+++ backend-devops/Controllers/HelloController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace backend_devops.Controllers
+{
+    [Route("api/[Controller]/")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public HelloController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [Route("greeting")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Greeting from backend. Great it's working!!!" });
+        }
+
+        [Route("dbcheck")]
+        [HttpGet]
+        public IActionResult TestDb()
+        {
+            var connString = _config.GetConnectionString("DefaultConnection");
+
+            using var conn = new NpgsqlConnection(connString);
+            conn.Open();
+
+            using var cmd = new NpgsqlCommand("SELECT version();", conn);
+            var version = cmd.ExecuteScalar()?.ToString();
+
+            return Ok(new { dbVersion = version });
+        }
+    }
+}
---
+++ frontend-devops/src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function App() {
+  const [message, setMessage] = useState('Loading...');
+
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_API_URL}/api/hello/greeting`)
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        setMessage('Error fetching message:' + error.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>React App</h1>
+      <p>Message: {message}</p>
+      <p>Frontend is working!</p>
+    </div>
+  );
+}
